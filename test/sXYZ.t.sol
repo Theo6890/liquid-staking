f98afffdf98f7658df87d9c4be4bb8e54b3dae0b
Théo RICHARD <mail@rcode.tech>
2023-07-06 23:10:26 +0200

    test: mock `claimRewards`

---
+++ test/sXYZ.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+
+import {IStakingXYZ} from "../src/IStakingXYZ.sol";
+
+import {sXYZ} from "../src/sXYZ.sol";
+
+contract sXYZ_Test is Test {
+    IStakingXYZ public stakingXYZ;
+    sXYZ public sxyz;
+
+    function setUp() public {
+        stakingXYZ = IStakingXYZ(address(0));
+        sxyz = new sXYZ(stakingXYZ);
+
+        // to avoid vm.mockCall issue on zero bytecode address
+        vm.etch(address(stakingXYZ), bytes("0x6080604052348015610010"));
+    }
+
+    function testMockCall_claimRewards() public {
+        assertEq(sxyz.total_staked_amount(), 0);
+
+        vm.mockCall(
+            address(stakingXYZ),
+            abi.encodeWithSelector(stakingXYZ.claimReward.selector),
+            abi.encode(10 ether)
+        );
+
+        sxyz.claimRewards();
+        assertEq(sxyz.total_staked_amount(), 10 ether);
+    }
+}

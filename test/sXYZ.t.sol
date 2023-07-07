139501bb32e942936c4507f71a3cddc62793eed3
Théo RICHARD <mail@rcode.tech>
2023-07-07 16:06:17 +0200

    test: deposit function

--- test/sXYZ.t.sol
+++ test/sXYZ.t.sol
@@ -13,6 +13,8 @@ contract sXYZ_Test is Test {
 
     sXYZ public sxyz;
 
+    uint256 public relayerFee = 0.01 ether;
+
     function setUp() public {
         stakingXYZ = IStakingXYZ(makeAddr("StakingXYZ"));
         validator = makeAddr("validator");
@@ -34,5 +36,36 @@ contract sXYZ_Test is Test {
 
         sxyz.claimRewards();
         assertEq(sxyz.total_staked_amount(), 10 ether);
+        // // when StakingXYZ will be implemented, sXYZ should receive 10 ether from StakingXYZ, through `fallback()`
+        // assertEq(payable(address(sxyz)).balance, 10 ether);
+    }
+
+    function test_deposit() public {
+        assertEq(payable(address(stakingXYZ)).balance, 0);
+
+        vm.mockCall(
+            address(stakingXYZ),
+            abi.encodeWithSelector(stakingXYZ.getRelayerFee.selector),
+            abi.encode(relayerFee)
+        );
+
+        sxyz.deposit{value: 10 ether}();
+
+        uint256 netDeposit = 10 ether - relayerFee;
+
+        assertEq(sxyz.total_staked_amount(), netDeposit);
+        assertEq(sxyz.balanceOf(address(this)), netDeposit);
+        assertEq(payable(address(stakingXYZ)).balance, 10 ether);
+    }
+
+    function testRevert_deposit_When_LowerThanRelayerFee() public {
+        vm.mockCall(
+            address(stakingXYZ),
+            abi.encodeWithSelector(stakingXYZ.getRelayerFee.selector),
+            abi.encode(relayerFee)
+        );
+
+        vm.expectRevert("sXYZ: deposit <= relayerFee");
+        sxyz.deposit{value: relayerFee - 1}();
     }
 }
